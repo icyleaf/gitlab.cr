c41917cb70b02c25b0d18cadeeeef835e5be9427
icyleaf <icyleaf.cn@gmail.com>
2016-06-19 08:35:51 +0800

    refactor: merged session api and update doc

--- src/gitlab/client/session.cr
+++ src/gitlab/client/session.cr
@@ -6,17 +6,16 @@ module Gitlab
     module Session
       # Creates a new user session.
       #
-      # **Note** This method doesn't require private_token to be set.
-      #
-      # - param  [String] email The email of a user.
+      # - param  [String] login The email or username of a user.
       # - param  [String] password The password of a user.
       # - return [Hash] Information about logged in user.
       #
       # ```
-      # client.session('jack@example.com', 'secret12345')
+      # client.session("jack@example.com", "secret12345")
+      # client.session("jack", "secret12345")
       # ```
-      def session(email, password)
-        post("/session", { "email" => email, "password" => password })
+      def session(login : String, password : String)
+        post("/session", { "email" => login, "password" => password }).body.parse_json
       end
     end
   end
