66fcfd7c65051016c92c41057593d51a6204bbc0
icyleaf <icyleaf.cn@gmail.com>
2017-09-19 13:50:05 +0800

    test: fix stub_put with form data

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -13,31 +13,30 @@ def load_fixture(name)
   File.read_lines(File.dirname(__FILE__) + "/fixtures/#{name}.json").join("\n")
 end
 
+# GET
 def stub_get(path, fixture)
   WebMock.stub(:get, "#{client.endpoint}#{path}")
          .with(headers: {"Private-Token" => client.token})
          .to_return(body: load_fixture(fixture))
 end
 
+# POST
 def stub_post(path, fixture, status_code = 200)
   WebMock.stub(:post, "#{client.endpoint}#{path}")
          .with(headers: {"Private-Token" => client.token})
          .to_return(body: load_fixture(fixture), status: status_code)
 end
 
-def stub_put(path, fixture, params = nil)
-  query = if params
-    "?" + params.map {|k,v| "#{k}=#{v}"}.join("&")
-  else
-   ""
-  end
+# PUT
+def stub_put(path, fixture, form = nil)
+  body = HTTP::Params.escape(form) if form
 
-  WebMock.stub(:put, "#{client.endpoint}#{path}#{query}")
-         .with(headers: {"Private-Token" => client.token})
+  WebMock.stub(:put, "#{client.endpoint}#{path}")
+         .with(body: body, headers: {"Private-Token" => client.token})
          .to_return(body: load_fixture(fixture))
 end
 
-
+# DELETE
 def stub_delete(path, fixture)
   WebMock.stub(:delete, "#{client.endpoint}#{path}")
          .with(headers: {"Private-Token" => client.token})
