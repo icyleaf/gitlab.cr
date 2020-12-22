dc0fe0241274dcc2f4d6d6612c2b65337b82f89c
icyleaf <icyleaf.cn@gmail.com>
2020-12-22 11:41:02 +0800

    spec: fix

--- spec/gitlab/client_spec.cr
+++ spec/gitlab/client_spec.cr
@@ -2,13 +2,13 @@ require "../spec_helper"
 
 describe Gitlab::Client do
   describe ".initialize" do
-    it "should initilize with api version under v1/v2/v3/v4" do
+    it "should initilize with api version" do
       Gitlab::Client.new(GITLAB_ENDPOINT, GITLAB_TOKEN).should be_a Gitlab::Client
     end
 
-    it "should throws an exception with v5 api" do
+    it "should throws an exception with graphql api" do
       expect_raises Gitlab::Error::NoSupportGraphQLAPIError do
-        Gitlab::Client.new("https://gitlab.example.com/api/v5", GITLAB_TOKEN)
+        Gitlab::Client.new("https://gitlab.example.com/api/graphql", GITLAB_TOKEN)
       end
     end
   end
